30133e7531b96fd4fd3096f25318e338af9386a6
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-07-21 01:45:33 +0700

    ADD: old login method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,12 +5,32 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function oldLogin()
     {
+        // $data = DB::select("SELECT * FROM `users` WHERE email = 'kari.orn@example.com' and password = 'barjono'");
+        $data = DB::table('users')->where('email', '=', 'kari.orn@example.com')->where('password', '=', 'barjono')->first();
+        // dd($data->id);
+        session(['email_user' => $data->email]);
+        // return $request->session()->all();
+        return response('congrats')->cookie('email_user', $data->email, 120);
+    }
+    public function oldDash(Request $request)
+    {
+        $session_email = $request->session()->get('email_user');
+        $cookie_email = $request->cookie('email_user');
+        $sts = $cookie_email == $session_email;
+        // return $sts;
         
+        if($sts){
+            return 'welcome to dashboard <a href="/logout" >logout</>';
+        }
+        else{
+            return 'bad login';
+        }
     }
     public function login(User $user)
     {
