a74f0495d6e49c75b1822bc7286f2f27536822a6
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-07-21 21:49:50 +0700

    UPDATE: user sidebar request active

--- resources/views/layouts/user/sidebar.blade.php
+++ resources/views/layouts/user/sidebar.blade.php
@@ -2,8 +2,8 @@
 
     <div id="mySidenav" class="sidenav">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="{{ route('user.dashboard') }}">Dashboard</a>
-        <a href="{{ route('user.sertifikat') }}">Sertifikat</a>
-        <a href="{{ route('user.pengaturan') }}">Pengaturan</a>
-        <a href="{{ route('user.logout') }}">Keluar</a>
+        <a class="{{request()->is('user/dashboard') ? 'active' : ''}}" href="{{ route('user.dashboard') }}">Dashboard</a>
+        <a class="{{request()->is('user/sertifikat') ? 'active' : ''}}" href="{{ route('user.sertifikat') }}">Sertifikat</a>
+        <a class="{{request()->is('user/pengaturan') ? 'active' : ''}}" href="{{ route('user.pengaturan') }}">Pengaturan</a>
+        <a href="{{ route('user.logout') }}">Keluar</a> 
     </div>
