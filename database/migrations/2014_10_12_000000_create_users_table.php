83639a588bc518e22fd710e8432e27f1d3f15e69
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-07-21 20:49:00 +0700

    ADD: current timestamp

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -25,8 +26,11 @@ class CreateUsersTable extends Migration
             $table->enum('gender', ['L', 'P']);
             $table->enum('agama', ['islam', 'kristen', 'katolik', 'hindu', 'budha', 'konghucu']);
             $table->rememberToken();
+            // $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('current_timestamp'));
+            $table->timestamp('updated_at')->default(DB::raw('current_timestamp on update current_timestamp'));
             $table->softDeletes();
-            $table->timestamps();
+
         });
     }
 
